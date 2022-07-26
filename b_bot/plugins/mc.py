ac5b87bd90edf1657a8604ee667868d49179b7ac
OneRain233 <onerain233@outlook.com>
2022-07-26 11:49:29 +0800

    poke msg

--- b_bot/plugins/mc.py
+++ b_bot/plugins/mc.py
@@ -5,12 +5,12 @@ from nonebot.adapters import Message
 from mcstatus import JavaServer
 from nonebot.rule import to_me
 from nonebot.params import Arg, CommandArg, ArgPlainText
-
+from nonebot.adapters.onebot.v11 import Event, PokeNotifyEvent
 # 戳一戳
 mc = on_notice(rule=to_me())
 
 @mc.handle()
-async def mc_status(bot: Bot, event: Event, state: T_State):
+async def mc_status(bot: Bot, event: PokeNotifyEvent, state: T_State):
     server = JavaServer(host="1.15.105.102", port=10001)
     status = server.status().latency
     msg = """
