dad7f823833fffd610344ab5ffd54bab210659af
OneRain233 <onerain233@outlook.com>
2022-07-26 21:39:32 +0800

    group increase event

--- b_bot/plugins/welcome.py
+++ b_bot/plugins/welcome.py
@@ -1,9 +1,11 @@
-from nonebot import on_command, on_startswith, on_notice
-from nonebot.typing import T_State
+from nonebot import on_notice
+from nonebot.adapters.onebot.v11.message import Message, MessageSegment
 from nonebot.adapters import Bot, Event
-from nonebot.adapters.onebot.v11 import GroupIncreaseNoticeEvent
-import base64
+from nonebot.adapters.onebot.v11 import Bot, GroupIncreaseNoticeEvent
 
+
+async def _group_increase(bot: Bot, event: Event) -> bool:
+    return isinstance(event, GroupIncreaseNoticeEvent)
 
 
 group_increase = on_notice(_group_increase, priority=10, block=True)
@@ -12,4 +14,3 @@ group_increase = on_notice(_group_increase, priority=10, block=True)
 @group_increase.handle()
 async def _group_increase(bot: Bot, event: GroupIncreaseNoticeEvent):
     return await group_increase.send(MessageSegment.at(event.get_user_id()) + "欢迎新朋友~")
-
