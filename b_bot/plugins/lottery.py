a8ee3eb82990fe174a9b1acbbd3aca32d236f8b8
OneRain233 <onerain233@outlook.com>
2022-07-26 22:50:11 +0800

    lottery

--- b_bot/plugins/lottery.py
+++ b_bot/plugins/lottery.py
@@ -10,7 +10,7 @@ lottery = on_command("lottery")
 
 @lottery.handle()
 async def lottery_receive(bot: Bot, event: Event, state: T_State):
-    members = await bot.get_group_member_list(event.group_id)
+    members = await bot.get_group_member_list()
     members = [member.user_id for member in members]
     winner = random.choice(members)
     lottery_msg = "恭喜{}获奖～".format(await bot.get_group_member_info(event.group_id, winner))
