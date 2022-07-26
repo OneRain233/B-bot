8d7de4fe7ea02fa2300b3caf062fdddcefc461bd
OneRain233 <onerain233@outlook.com>
2022-07-26 23:04:01 +0800

    bug fix

--- b_bot/plugins/lottery.py
+++ b_bot/plugins/lottery.py
@@ -10,9 +10,14 @@ lottery = on_command("lottery")
 
 @lottery.handle()
 async def lottery_receive(bot: Bot, event: Event, state: T_State):
-    members = await bot.get_group_member_list()
-    members = [member.user_id for member in members]
+    # members = await bot.get_group_member_list(event.group_id)
+    members = await bot.call_api('get_group_member_list', group_id=event.group_id)
+    # print(members)
+    #return
+    # members = [member.user_id for member in members]
+    members = [member['user_id'] for member in members]
     winner = random.choice(members)
-    lottery_msg = "恭喜{}获奖～".format(await bot.get_group_member_info(event.group_id, winner))
+    lottery_msg = "恭喜{}获奖～".format(await bot.call_api('get_group_member_info', group_id=event.group_id, user_id=winner)['user_id'])
+    # lottery_msg = "恭喜{}获奖～".format(await bot.get_group_member_info(event.group_id, winner))
     await lottery.send(lottery_msg)
     
