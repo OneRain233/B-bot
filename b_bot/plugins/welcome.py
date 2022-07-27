7895ff4e11172be9d0c237aac26a3fbd5ab8d9cb
OneRain233 <onerain233@outlook.com>
2022-07-27 10:28:35 +0800

    welcome fix

--- b_bot/plugins/welcome.py
+++ b_bot/plugins/welcome.py
@@ -36,20 +36,37 @@ async def _seting_handle(bot: Bot, event: Event, state: T_State):
     args = event.message.extract_plain_text().split(' ')
     status = args[1]
     if status == "on":
-        filestream = open(os.path.join(resource_dir, 'config.json'), 'w').readall()
-        j = json.loads(filestream)
-        j["group_id"].append(event.group_id)
-        filestream.write(json.dumps(j))
-        
-        await setting.send("欢迎开关已开启")
+        with open(os.path.join(resource_dir, 'config.json'), 'r') as f:
+            
+            j = json.load(f)
+            if event.group_id not in j['group_id']:
+                j["group_id"].append(event.group_id)
+                with open(os.path.join(resource_dir, 'config.json'), 'w') as f:
+                    f.write(json.dumps(j))
+                await bot.send(event, "欢迎开关已开启")
+            else:
+                await bot.send(event, "欢迎开关已开启")
     elif status == "off":
-        filestream = open(os.path.join(resource_dir, 'config.json'), 'w')
-        j = json.loads(filestream)
-        j["group_id"].remove(event.group_id)
-        filestream.write(json.dumps(j))
+        with open(os.path.join(resource_dir, 'config.json'), 'r') as f:
+            j = json.load(f)
+            if event.group_id in j['group_id']:
+                j["group_id"].remove(event.group_id)
+                with open(os.path.join(resource_dir, 'config.json'), 'w') as f:
+                    f.write(json.dumps(j))
+                await bot.send(event, "欢迎开关已关闭")
+            else:
+                await bot.send(event, "欢迎开关已关闭")
         
-    
-    
+    elif status == "test":
+        welcome_config = json.loads(open(os.path.join(resource_dir, 'config.json'), 'r', encoding='utf-8').read())
+        
+        group_id = event.group_id
+        if group_id not in welcome_config['group_id']:
+            return
+        
+        msg = welcome_config['welcome_msg']
+        # msg = "欢迎新朋友加入网络安全协会～\n宣传网页:http://swjtunsa.com/intro\n问卷：https://docs.qq.com/form/page/DQU95T2h1dHR4VVFO"
+        await bot.send(event, msg)
     
 @group_increase.handle()
 async def _group_increase(bot: Bot, event: GroupIncreaseNoticeEvent):
