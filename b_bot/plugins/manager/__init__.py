262c1b3b450271e01d1ec7a4279e9570e348876f
onerain233 <onerain233@onerain233.onerain233>
2022-07-30 16:25:28 +0800

    cancle send

--- b_bot/plugins/manager/__init__.py
+++ b_bot/plugins/manager/__init__.py
@@ -31,19 +31,50 @@ get_group_list = on_command("groups",permission=SUPERUSER, aliases={'群列表'}
 exit_group = on_command("exit", permission=SUPERUSER, aliases={'退群'})
 
 @send_msg_monitor.handle()
-async def handle_first_receive(matcher: Matcher, args: Message = CommandArg()):
-    plain_text = args.extract_plain_text() 
+async def handle_first_receive(bot:Bot,matcher: Matcher, args: Message = CommandArg()):
+    plain_text = args.extract_plain_text()
+    arggs = plain_text.split(' ')
+    if len(arggs) >= 3:
+        msg = arggs[2:]
+        msg = ' '.join(msg)
+        type_ = arggs[0]
+        user_id = arggs[1]
+
+        userlist = await bot.call_api('get_friend_list')
+        grouplist = await bot.call_api('get_group_list')
+        userlist = [user['user_id'] for user in userlist]
+        grouplist = [user['group_id'] for user in grouplist]
+        if int(user_id) not in userlist and int(user_id) not in grouplist:
+            await send_msg_monitor.send("userlist\n" + str(userlist))
+            await send_msg_monitor.send("grouplist\n"+str(grouplist))
+            await send_msg_monitor.finish("请输入正确的用户或群组qq")
+        
+        if "群聊" in str(type_):
+            # await send_msg_monitor.send("111")
+            await bot.call_api("send_group_msg", group_id=user_id, message=(msg))
+        elif "私聊" in str(type_):
+            # await send_msg_monitor.send("222")
+            await bot.call_api('send_private_msg', user_id=user_id, message=(msg))
+            
+        await send_msg_monitor.finish(f"已经向{user_id}发送{msg}")
+
+
+    await send_msg_monitor.send(plain_text)
     if plain_text:
         matcher.set_arg("msg", args) 
     
 # 取得userid 和 msg
 @send_msg_monitor.got("msg", prompt="请输入要发送的消息")
 async def handle_msg(matcher: Matcher, msg: Message = Arg(), msg_txt: str = ArgPlainText("msg")):
+    if str(msg) == 'cancle':
+        await send_msg_monitor.finish("取消发送")
     matcher.set_arg("msg", msg)
     await send_msg_monitor.send(msg)
     
 @send_msg_monitor.got("type", prompt="请输入要发送的类型 1:群聊 2:私聊")
 async def handle_type(matcher: Matcher, _type: Message = Arg(), type_txt: str = ArgPlainText("type")):
+    if str(type_txt) == 'cancle':
+        await send_msg_monitor.finish("取消发送")
     if type_txt not in ['1', '2', '群聊', '私聊']:
         await send_msg_monitor.reject("请输入正确的类型")  
     
@@ -53,14 +84,16 @@ async def handle_type(matcher: Matcher, _type: Message = Arg(), type_txt: str = 
     
 @send_msg_monitor.got("user", prompt="请输入要发送的用户或群组qq")
 async def handle_user(bot: Bot, matcher: Matcher, user: Message = Arg(), user_id: str = ArgPlainText("user")):
+    if str(user) == 'cancle':
+        await send_msg_monitor.finish("取消发送")
     await send_msg_monitor.send(user)
     userlist = await bot.call_api('get_friend_list')
     grouplist = await bot.call_api('get_group_list')
     userlist = [user['user_id'] for user in userlist]
     grouplist = [user['group_id'] for user in grouplist]
     if int(user_id) not in userlist and int(user_id) not in grouplist:
-        await send_msg_monitor.send(str(userlist))
-        await send_msg_monitor.send(str(grouplist))
+        await send_msg_monitor.send("userlist\n" + str(userlist))
+        await send_msg_monitor.send("grouplist\n"+str(grouplist))
         await send_msg_monitor.reject("请输入正确的用户或群组qq")
     
     msg_txt = matcher.get_arg("msg")
@@ -103,3 +136,4 @@ async def exit_group_got(bot: Bot, matcher: Matcher, group_id: str = ArgPlainTex
     await bot.call_api('set_group_leave', group_id=group_id)
     await exit_group.finish(f"已经退出{group_id}")
     
+
