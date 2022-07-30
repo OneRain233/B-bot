c562eba6134a5a86f40204acfd6a78ac206427f8
OneRain233 <onerain233@outlook.com>
2022-07-30 15:19:28 +0800

    bug fix

--- b_bot/plugins/manager/__init__.py
+++ b_bot/plugins/manager/__init__.py
@@ -1,3 +1,4 @@
+from tokenize import group
 from nonebot import get_driver, on_command, get_bot
 from nonebot.permission import SUPERUSER
 from .config import Config
@@ -31,9 +32,9 @@ exit_group = on_command("exit", permission=SUPERUSER, aliases={'退群'})
 
 @send_msg_monitor.handle()
 async def handle_first_receive(matcher: Matcher, args: Message = CommandArg()):
-    plain_text = args.extract_plain_text()  # 首次发送命令时跟随的参数，例：/天气 上海，则args为上海
+    plain_text = args.extract_plain_text() 
     if plain_text:
-        matcher.set_arg("msg", args)  # 如果用户发送了参数则直接赋值
+        matcher.set_arg("msg", args) 
     
 # 取得userid 和 msg
 @send_msg_monitor.got("msg", prompt="请输入要发送的消息")
@@ -43,7 +44,7 @@ async def handle_msg(matcher: Matcher, msg: Message = Arg(), msg_txt: str = ArgP
     
 @send_msg_monitor.got("type", prompt="请输入要发送的类型 1:群聊 2:私聊")
 async def handle_type(matcher: Matcher, _type: Message = Arg(), type_txt: str = ArgPlainText("type")):
-    if type_txt not in ['1', '2']:
+    if type_txt not in ['1', '2', '群聊', '私聊']:
         await send_msg_monitor.reject("请输入正确的类型")  
     
     matcher.set_arg("type", _type)
@@ -56,16 +57,21 @@ async def handle_user(bot: Bot, matcher: Matcher, user: Message = Arg(), user_id
     userlist = await bot.call_api('get_friend_list')
     grouplist = await bot.call_api('get_group_list')
     userlist = [user['user_id'] for user in userlist]
+    grouplist = [user['group_id'] for user in grouplist]
     if int(user_id) not in userlist and int(user_id) not in grouplist:
         await send_msg_monitor.send(str(userlist))
+        await send_msg_monitor.send(str(grouplist))
         await send_msg_monitor.reject("请输入正确的用户或群组qq")
     
     msg_txt = matcher.get_arg("msg")
     type_ = matcher.get_arg("type")
     # send private msg
-    if type_ == "1":
+    await send_msg_monitor.send(type_)
+    if "群聊" in str(type_):
+        # await send_msg_monitor.send("111")
         await bot.call_api("send_group_msg", group_id=user_id, message=msg_txt)
-    else:
+    elif "私聊" in str(type_):
+        # await send_msg_monitor.send("222")
         await bot.call_api('send_private_msg', user_id=user_id, message=msg_txt)
         
     await send_msg_monitor.finish(f"已经向{user_id}发送{msg_txt}")
@@ -97,8 +103,3 @@ async def exit_group_got(bot: Bot, matcher: Matcher, group_id: str = ArgPlainTex
     await bot.call_api('set_group_leave', group_id=group_id)
     await exit_group.finish(f"已经退出{group_id}")
     
-@join_group.handle()
-async def join_group_handle(bot: Bot, event: Event, matcher: Matcher, args: Message = CommandArg()):
-    if args:
-        matcher.set_arg("group_id", args)
-
