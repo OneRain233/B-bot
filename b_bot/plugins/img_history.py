94db6c6b270404154d93ff6a0a51c3b4b8629a1d
OneRain2333 <onerain233@outlook.com>
2022-08-01 14:17:40 +0800

    add upload

--- b_bot/plugins/img_history.py
+++ b_bot/plugins/img_history.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from nonebot import on_notice, on_command, permission, CommandGroup
 from nonebot.adapters.onebot.v11.message import Message, MessageSegment
 from nonebot.adapters import Bot, Event
@@ -8,8 +9,10 @@ import json
 from .pic_gen import img_to_b64
 from PIL import Image, ImageDraw, ImageFont
 import random
+import requests
 
 img_history = on_command("img_history", aliases={"图片历史"})
+upload_img = on_command("upload_img", aliases={"上传图片"})
 
 # resources/img/
 resource_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources', 'img')
@@ -24,3 +27,30 @@ async def img_handle(bot: Bot, event: Event, state:T_State):
     img_file = os.path.join(resource_dir, img)
     img = Image.open(img_file)
     await bot.send(event, MessageSegment.image(img_to_b64(img)))
+
+async def download_img(url):
+    try:
+        r = requests.get(url)
+        # save to resources/img/
+        filename = str(random.randint(0, 1000000)) + '.png'
+        filepath = os.path.join(resource_dir, filename)
+        with open(filepath, 'wb') as f:
+            f.write(r.content)
+        return filename
+    except Exception as e:
+        print(e)
+        return False
+
+@upload_img.handle()
+async def upload_img_handle(bot: Bot, event: Event, state:T_State):
+    msg = event.get_message()
+    msg_seg: MessageSegment = msg[1]
+    img_url = msg_seg.data['url']
+    filename = await download_img(img_url)
+    if filename:
+        img_url = "file://" + os.path.join(resource_dir, filename)
+        await bot.send(event, MessageSegment.image(img_url))
+    else:
+        await bot.send(event, "上传失败")
+
+
